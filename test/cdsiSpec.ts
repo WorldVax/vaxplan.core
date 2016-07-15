62382db1e6baea92ef68dd70c0bd0ae3cbf0dcb9
Miller <jmiller@icct.com>
2016-07-14 20:24:18 -0400

    Converted CDSI test file to use jasmine style

---
+++ test/cdsiSpec.ts
@@ -0,0 +1,50 @@
+/// <reference path="../typings/index.d.ts" />
+
+import Cdsi = require("../lib/cdsi");
+
+describe("The Can Evaluate Administered Dose decision", () => {
+    it("should report false when there is a Dose Condition indicated", () => {
+        var canEvaluateDecision = new Cdsi.Decision_CanEvaluateAdministeredDose();
+        let context: Cdsi.IDecisionContext_AdministeredDoseCanBeEvaluated;
+        context = {
+            doseCondition: "Spoiled",
+            lotExpirationDate: new Date("2015-12-31"),
+            administeredDate: new Date("2016-05-01")
+        };
+
+        let result: Cdsi.IDecisionOutcome_AdministeredDoseCanBeEvaluated;
+        result = canEvaluateDecision.decide(context);
+        expect(result).toBeDefined();
+        expect(result.canBeEvaluated).toEqual(false);
+    });
+
+    it("should report false when the Administered Date is after the Lot Expiration Date", () => {
+        var canEvaluateDecision = new Cdsi.Decision_CanEvaluateAdministeredDose();
+        let context: Cdsi.IDecisionContext_AdministeredDoseCanBeEvaluated;
+        context = {
+            doseCondition: null,
+            lotExpirationDate: new Date("2015-12-31"),
+            administeredDate: new Date("2016-05-01")
+        };
+
+        let result: Cdsi.IDecisionOutcome_AdministeredDoseCanBeEvaluated;
+        result = canEvaluateDecision.decide(context);
+        expect(result).toBeDefined();
+        expect(result.canBeEvaluated).toEqual(false);
+    });
+
+    it("should report true when no rules are broken", () => {
+        var canEvaluateDecision = new Cdsi.Decision_CanEvaluateAdministeredDose();
+        let context: Cdsi.IDecisionContext_AdministeredDoseCanBeEvaluated;
+        context = {
+            doseCondition: null,
+            lotExpirationDate: new Date("2027-12-31"),
+            administeredDate: new Date("2016-05-01")
+        };
+
+        let result: Cdsi.IDecisionOutcome_AdministeredDoseCanBeEvaluated;
+        result = canEvaluateDecision.decide(context);
+        expect(result).toBeDefined();
+        expect(result.canBeEvaluated).toEqual(true);
+    });
+})
